fa790b1bf6df9f13a5be811c98cc5298120e56ad
Yury Hamaniuk <yury_hamaniuk@epam.com>
2020-08-14 21:58:59 +0300

    fix tests

--- src/test/first_test.ts
+++ src/test/first_test.ts
@@ -5,21 +5,20 @@ import HomePage from '../lib/pages/home/home_page';
 import LoginPage from '../lib/pages/login/login_page';
 import ToDoPage from '../lib/pages/todo/todo_page';
 
-fixture('home page fixture').page('https://todo.microsoft.com/tasks/');
+fixture('home page fixture').page(Data.applicationUrl);
 
 const homePage = pages('Home') as HomePage;
 const loginPage = pages('Login') as LoginPage;
 const todoPage = pages('ToDo') as ToDoPage;
 
-test('check home page title', async t => {
-  await t
-  .maximizeWindow()
-  .expect(homePage.title().textContent).eql('Microsoft To Dr');
+test('check home page title', async test => {
+  await test
+    .maximizeWindow()
+    .expect(homePage.title().textContent).eql('Microsoft To Do');
 });
- 
 
- test('check that user can login', async t => 
-  await t     
+test('check that user can login', async test => 
+  await test     
     .click(homePage.buttonByName('Get started'))
     .typeText(loginPage.inputByType('email'), Data.login)
     .click(loginPage.inputByType('submit'))
